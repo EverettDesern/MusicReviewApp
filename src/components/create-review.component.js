c91d4d4a75fc12a3cf8c8c54ee2aa2ba746af50a
Everett Desern <edesern@live.unc.edu>
2020-07-12 13:27:31 -0400

    Updates

--- src/components/create-review.component.js
+++ src/components/create-review.component.js
@@ -2,6 +2,68 @@ import React, { Component } from 'react';
 
 export default class CreateReview extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeArtist = this.onChangeArtist.bind(this);
+        this.onChangeAlbum = this.onChangeAlbum.bind(this);
+        this.onChangeReview = this.onChangeReview.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            username: "",
+            artist: "",
+            album: "",
+            review: "",
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            users: ['test'],
+            username: 'test'
+        });
+    }
+
+    onChangeUsername(e) {
+        this.setState( {
+            username: e.target.value
+        });
+    }
+    onChangeArtist(e) {
+        this.setState( {
+            artist: e.target.value
+        });
+    }
+    onChangeAlbum(e) {
+        this.setState( {
+            album: e.target.value
+        });
+    }
+    onChangeReview(e) {
+        this.setState( {
+            review: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const review = {
+            username: this.state.username,
+            artist: this.state.artist,
+            album: this.state.album,
+            review: this.state.review
+        }
+
+        console.log(review);
+
+        window.location = '/';
+    }
+
+
     render() {
         return (
             <div>
